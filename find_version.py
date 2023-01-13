74ff292c9834aaccb16e1e88d4e2b721b59c5a60
richeek05 <114706858+richeek05@users.noreply.github.com>
2023-01-13 16:46:26 -0700

    Update find_version.py

--- find_version.py
+++ find_version.py
@@ -5,7 +5,7 @@ print(requests.__version__)
 url = 'https://raw.githubusercontent.com/richeek05/Cmput404-Labs/main/find_version.py'
 r = requests.get(url, allow_redirects=True)
 
-open('Python Code', 'wb').write(r.content)
+open('PythonCode', 'wb').write(r.content)
 
 print('The file was saved successfully')
 
